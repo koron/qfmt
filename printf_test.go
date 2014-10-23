96ad04a6a2724c782935ff6243c7215633fa80ec
MURAOKA Taro <koron.kaoriya@gmail.com>
2014-10-23 18:15:55 +0900

    more/refactor tests

--- printf_test.go
+++ printf_test.go
@@ -4,18 +4,27 @@ import (
 	"testing"
 )
 
-func TestFormatConst(t *testing.T) {
-	var s string
-	s = Sprintf("Hello World")
-	if "Hello World" != s {
-		t.Errorf(`TestFormatConst is failed: %#v`, s)
+func formatCheck(t *testing.T, expected, fmt string, a ...interface{}) {
+	s := Sprintf(fmt, a...)
+	if s != expected {
+		t.Errorf("%#v isn't expaned to %#v: %#v", fmt, expected, s)
 	}
 }
 
+func TestFormatConst(t *testing.T) {
+	formatCheck(t, "Hello World", "Hello World")
+}
+
 func TestFormatString(t *testing.T) {
-	var s string
-	s = Sprintf("%s", "foobar")
-	if "foobar" != s {
-		t.Errorf(`%%s is not "foobar": %#v`, s)
-	}
+	formatCheck(t, "contents", "%s", "contents")
+	formatCheck(t, "PREFIX contents SUFFIX", "PREFIX %s SUFFIX", "contents")
+	formatCheck(t, "contents SUFFIX", "%s SUFFIX", "contents")
+	formatCheck(t, "PREFIX contents", "PREFIX %s", "contents")
+}
+
+func TestFormatInt32(t *testing.T) {
+	formatCheck(t, "1234567890", "%d", 1234567890)
+	formatCheck(t, "PREFIX 1234567890 SUFFIX", "PREFIX %d SUFFIX", 1234567890)
+	formatCheck(t, "1234567890 SUFFIX", "%d SUFFIX", 1234567890)
+	formatCheck(t, "PREFIX 1234567890", "PREFIX %d", 1234567890)
 }
