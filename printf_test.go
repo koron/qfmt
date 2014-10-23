69672fd208c58095b5448f6bb1d44da1c2ac75ac
MURAOKA Taro <koron.kaoriya@gmail.com>
2014-10-23 23:28:25 +0900

    more benchmarks

--- printf_test.go
+++ printf_test.go
@@ -2,8 +2,87 @@ package qfmt
 
 import (
 	"fmt"
+	"io/ioutil"
 	"testing"
 )
+
+////////////////////////////////////////////////////////////////////////////
+// Benchmark
+
+func BenchmarkQfmtFprintfConst(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Fprintf(ioutil.Discard, "Hello World")
+	}
+}
+
+func BenchmarkFmtFprintfConst(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		fmt.Fprintf(ioutil.Discard, "Hello World")
+	}
+}
+
+func BenchmarkQfmtFprintfString(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Fprintf(ioutil.Discard, "PREFIX %s SUFFIX", "contents")
+	}
+}
+
+func BenchmarkFmtFprintfString(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		fmt.Fprintf(ioutil.Discard, "PREFIX %s SUFFIX", "contents")
+	}
+}
+
+func BenchmarkQfmtFprintfInt(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Fprintf(ioutil.Discard, "PREFIX %d SUFFIX", 1234567890)
+	}
+}
+
+func BenchmarkFmtFprintfInt(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		fmt.Fprintf(ioutil.Discard, "PREFIX %d SUFFIX", 1234567890)
+	}
+}
+
+func BenchmarkQfmtSprintfConst(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Sprintf("Hello World")
+	}
+}
+
+func BenchmarkFmtSprintfConst(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		fmt.Sprintf("Hello World")
+	}
+}
+
+func BenchmarkQfmtSprintfString(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Sprintf("PREFIX %s SUFFIX", "contents")
+	}
+}
+
+func BenchmarkFmtSprintfString(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		fmt.Sprintf("PREFIX %s SUFFIX", "contents")
+	}
+}
+
+func BenchmarkQfmtSprintfInt(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Sprintf("PREFIX %d SUFFIX", 1234567890)
+	}
+}
+
+func BenchmarkFmtSprintfInt(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		fmt.Sprintf("PREFIX %d SUFFIX", 1234567890)
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////
+// Tests
 
 func formatCheck(t *testing.T, expected, fmt string, a ...interface{}) {
 	s := Sprintf(fmt, a...)
@@ -16,18 +95,6 @@ func TestFormatConst(t *testing.T) {
 	formatCheck(t, "Hello World", "Hello World")
 }
 
-func BenchmarkQfmtConst(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		Sprintf("Hello World")
-	}
-}
-
-func BenchmarkFmtConst(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		fmt.Sprintf("Hello World")
-	}
-}
-
 func TestFormatString(t *testing.T) {
 	formatCheck(t, "contents", "%s", "contents")
 	formatCheck(t, "PREFIX contents SUFFIX", "PREFIX %s SUFFIX", "contents")
@@ -35,33 +102,9 @@ func TestFormatString(t *testing.T) {
 	formatCheck(t, "PREFIX contents", "PREFIX %s", "contents")
 }
 
-func BenchmarkQfmtString(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		Sprintf("PREFIX %s SUFFIX", "contents")
-	}
-}
-
-func BenchmarkFmtString(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		fmt.Sprintf("PREFIX %s SUFFIX", "contents")
-	}
-}
-
-func TestFormatInt32(t *testing.T) {
+func TestFormatInt(t *testing.T) {
 	formatCheck(t, "1234567890", "%d", 1234567890)
 	formatCheck(t, "PREFIX 1234567890 SUFFIX", "PREFIX %d SUFFIX", 1234567890)
 	formatCheck(t, "1234567890 SUFFIX", "%d SUFFIX", 1234567890)
 	formatCheck(t, "PREFIX 1234567890", "PREFIX %d", 1234567890)
-}
-
-func BenchmarkQfmtInt32(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		Sprintf("PREFIX %d SUFFIX", 1234567890)
-	}
-}
-
-func BenchmarkFmtInt32(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		fmt.Sprintf("PREFIX %d SUFFIX", 1234567890)
-	}
 }
